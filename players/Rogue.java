fbff905f439148fbacd68f4befe899f98a989b72
Teo <teo_matei1999@yahoo.com>
2019-12-01 09:45:43 +0200

    Updated Rogue

--- players/Rogue.java
+++ players/Rogue.java
@@ -1,7 +1,30 @@
 package players;
 
 import skills.AttackVisitor;
+import skills.RogueSkills;
 import utils.Coordinates;
 
 public class Rogue extends Hero {
+    public Rogue(Coordinates c) {
+        super(c);
+        this.setHp(600);
+        this.setMaxHp(600);
+    }
+
+    @Override
+    public int getMaxHpLevelUp() {
+        return 600 + this.getLevel() * 40;
+    }
+
+    @Override
+    public void accept(AttackVisitor skill) {
+        skill.visit(this);
+    }
+
+    @Override
+    public AttackVisitor heroSkill() {
+        RogueSkills rogueSkills = new RogueSkills(this);
+        super.setBackstab(super.getBackstab() + 1);
+        return rogueSkills;
+    }
 }
