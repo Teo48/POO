c84b5fd18e13cac0f63a4be9c17d4ab7aec1c5a9
Teo <teo_matei1999@yahoo.com>
2019-12-01 10:34:30 +0200

    Added Knight

--- players/Knight.java
+++ players/Knight.java
@@ -1,7 +1,28 @@
 package players;
 
 import skills.AttackVisitor;
+import skills.KnightSkills;
 import utils.Coordinates;
 
 public class Knight extends Hero {
+    public Knight(Coordinates c) {
+        super(c);
+        this.setHp(900);
+        this.setMaxHp(900);
+    }
+
+    @Override
+    public int getMaxHpLevelUp() {
+        return 900 + 80 * this.getLevel();
+    }
+
+    @Override
+    public void accept(AttackVisitor skill) {
+        skill.visit(this);
+    }
+
+    @Override
+    public AttackVisitor heroSkill() {
+        return new KnightSkills(this);
+    }
 }
